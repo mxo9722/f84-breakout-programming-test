981b18f097a4bed6947d1aca8c21591a71516890
mxo9722 <mxo9722@rit.edu>
2021-02-12 19:05:52 -0500

    Finished adding everything to the splash screen
    All the visuals for the title screen have been added. Still needs the high score board and play button to be fully implemented.

--- src/breakout/scenes/SplashScene.js
+++ src/breakout/scenes/SplashScene.js
@@ -1,6 +1,7 @@
 import Scene from '../../core/Scene.js';
 import Image from '../../core/components/Image.js';
 import Entity from '../../core/Entity.js';
+import TextField, { TextFieldConfig, TextAlign } from '../../core/components/TextField.js';
 
 export default class SplashScene extends Scene
 {
@@ -16,8 +17,26 @@ export default class SplashScene extends Scene
         const backgroundEntity = new Entity(this, 1024/2.0, 576/2.0)
         new Image(backgroundEntity, "splashBackground")
 
-        const gameTitle = new Entity(this,1024/2,576/2)
+        const gameTitle = new Entity(this,1024/2,576/2-160)
         new Image(gameTitle,"gameTitle")
+
+        const dashLine = new Entity(this, 1024/2, 576/2-110)
+        new Image(dashLine,"splashLine")
+
+        //TODO: Implement high score system
+        const highScore = new Entity(this, 1024/2.0-30, 576/2.0 - 80)
+        new TextField(highScore, "High Score: \t", new TextFieldConfig("Arial", "14px", "#FFFFFF", TextAlign.CENTER))
+
+        const playBtn = new Entity(this,1024/2,576/2+160);
+        new Image(playBtn,"btnPurple")
+
+        const playBtnText = new Entity(this,0,15)
+        playBtn.addChild(playBtnText)
+        new TextField(playBtnText,"Play",new TextFieldConfig("Arial", "42px", "#FFFFFF", TextAlign.CENTER))
+
+        const playBtnSymbol = new Entity(this,-100,0)
+        playBtn.addChild(playBtnSymbol)
+        new Image(playBtnSymbol,"playIcon")
     }
 
     update(delta)
